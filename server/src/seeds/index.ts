9bd7a20740b7413da405ea37079a65bc60a03ca6
mmakel <makel.matthew@gmail.com>
2025-01-04 20:18:11 -0600

    Add database seeding functionality with SQL file execution

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -1,9 +1,34 @@
 import { seedUsers } from './user-seeds.js';
 import { seedTickets } from './ticket-seeds.js';
 import { sequelize } from '../models/index.js';
+import dotenv from 'dotenv';
+import { exec } from 'child_process';
+
+dotenv.config();
+
+const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+const runSQLFile = (filePath: string) => {
+  return new Promise((resolve, reject) => {
+    const command = `psql postgresql://${DB_USER}:${DB_PASSWORD}@localhost/${DB_NAME} -f ${filePath}`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing command: ${command}`);
+        console.error(stderr);
+        reject(error);
+      } else {
+        console.log(stdout);
+        resolve(stdout);
+      }
+    });
+  });
+};
 
 const seedAll = async (): Promise<void> => {
   try {
+    await runSQLFile('./server/src/seeds/seed.sql');
+    console.log('\n----- DATABASE INITIALIZED -----\n');
+
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
     
